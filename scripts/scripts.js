9769b81352802b637c1765a2731650e9ef368b37
Bianca Blakesley <bianca.blakesley@gmail.com>
2022-03-18 19:03:59 -0400

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,92 @@
+/* PROBLEM #1.0 - Create an object called car. This object has 5 keys with values:
+    make: String, 
+    model: String, 
+    colors: Array (include 3 items), 
+    year: Number,
+    isAutomatic: Boolean 
+ You can fill in the values to represent your dream car or the car you currently drive. */
+
+
+// PROBLEM #1.1 - Have the following message appear in the console.
+// "I think a [COLOR] [MAKE] [MODEL] are really nice cars." 
+// You should be getting the values from the color, make, and model keys in the object.
+
+// PROBLEM #1.2 - Add a new key called hasSiriusXM and give it a value.
+
+// PROBLEM #1.3 - Update the year key with a new value.
+
+// PROBLEM #1.4 - Delete the isAutomatic key.
+
+
+
+
+/* PROBLEM #2.0 - Create an object called movie. This object has 5 keys with values:
+    title: String, 
+    director: String, 
+    actors: Array (include 3 items), 
+    inTheaters: Boolean,
+    runtime: Number
+ You can fill in the values to represent your favorite movie. */
+
+
+// PROBLEM #2.1 - Have the following message appear in the console.
+// "In my opinion, [TITLE] by [DIRECTOR] is a must watch!" 
+// You should be getting the values from the title, and director keys in the object.
+
+// PROBLEM #2.2 - Add a new key called whereToWatch and give it 3 String values.
+
+// PROBLEM #2.3 - Update the actor key by adding a 4th actor
+
+// PROBLEM #2.4 - Delete the inTheaters key.
+
+
+
+
+/* PROBLEM #3.0 - Create an object called book. This object has 4 keys with values:
+    title: String, 
+    author: String, 
+    genre: Array (include 3 items),
+    isSoldOut: Boolean, 
+    numOfPages: Number
+ You can fill in the values to represent your favorite book. */
+
+
+// PROBLEM #3.1 - Have the following message appear in the console.
+// "This month's book is [TITLE] by [AUTHOR]." 
+// You should be getting the values from the title, and director keys in the object.
+
+// PROBLEM #3.2 - Add a new key called publisher and give it a String value.
+
+// PROBLEM #3.3 - Update the isSoldOut key by giving it the opposite value.
+
+// PROBLEM #3.4 - Delete the numOfPages key.
+
+
+
+
+/* PROBLEM #4.0 - Create an object called person This object has 5 keys with values:
+    firstName: String, 
+    age: Number, 
+    city, String
+    hasPet: Boolean, 
+    favoriteFood: Array (include 3 items)
+ You can fill in the values to represent yourself. */
+
+
+// PROBLEM #4.1 - Have the following message appear in the console.
+// "Hello! My name is [FIRSTNAME] and I love in [CITY]." 
+// You should be getting the values from the first name, and city keys in the object.
+
+// PROBLEM #4.2 - Add a new key called favoriteColors and give it 3 String values.
+
+// PROBLEM #4.3 - Update the city key with a new value.
+
+// PROBLEM #4.4 - Delete the hasPet key.
+
+
+
+/* PROBLEM #5.0 - Create your own object! Make sure that you can:
+  1: access the values within the object and display via console
+  2: add a new key/value to the object
+  3: update a key with a new value
+  4: delete a key from the object */
